440e73359001e525eb56530b61daa3cd061b2da8
Alfonso Mancilla Alvis <almancill@gmail.com>
2015-06-23 01:31:26 -0500

    setup and define Review and Show models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,19 @@
 class Review < ActiveRecord::Base
-  belongs_to :show
+
+  # Associations
+  belongs_to :show # Perform the counter cache manually on the :after_create callback to avoid and extra query to the db.
+
+  # Validations
+  validates :show_id, :critic_name, :score, presence: true
+  validates :show_id, uniqueness: { scope: :critic_name }
+  validates :score, numericality: { greater_than_or_equal_to: 0 }
+
+  # Callbacks
+  after_create :update_show_average_rating
+
+  # Methods
+  private
+    def update_show_average_rating
+      self.show.update_reviews_average(self.score)
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,25 @@
 class Show < ActiveRecord::Base
-  has_many :reviews
+
+  # Scopes
+  scope :top_ten, -> { order(reviews_average: :desc).limit(10) }
+
+  # Associations
+  has_many :reviews, dependent: :delete_all
+
+  # Validations
+  validates :title, presence: true
+
+  # Methods
+  # Update the reviews average and the reviews count given a new score
+  def update_reviews_average(score)
+    self.reviews_average = calculate_reviews_average(score)
+    self.reviews_count += 1
+    self.save
+  end
+
+  private
+    # Calculate the new reviews average based on the current average and the new score
+    def calculate_reviews_average(score)
+      (self.reviews_average * self.reviews_count + score) / (self.reviews_count + 1)
+    end
 end
